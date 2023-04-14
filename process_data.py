27554ee741a87db115fd96ae99517604862ece67
iaroslavm <iaroslav.miller@gmail.com>
2023-04-13 22:06:31 -0400

    comment 2: adjusting the ETL pipeline, adding load, clean and save methods

--- process_data.py
+++ process_data.py
@@ -1,15 +1,44 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath).drop_duplicates()
+    categories = pd.read_csv('categories.csv').drop_duplicates()
+
+    # merge datasets
+    df = messages.merge(categories, how='inner', on='id').drop_duplicates()
+    return df
 
 
 def clean_data(df):
-    pass
+
+    # find names of categories
+    categories_list = [cat.split('-')[0] for cat in df['categories'].values[0].split(';')]
+    # expand categories into separate columns
+    df[categories_list] = df['categories'].str.split(';', expand=True)
+    # keep only boolean values in categories
+    for column in categories_list:
+        # set each value to be the last character of the string
+        df[column] = df[column].str.split('-', expand=True).iloc[:, 1]
+
+        # convert column from string to numeric
+        df[column] = df[column].astype(int)
+
+    # drop initial categories column and remove duplicates
+    df = df.drop(['categories'], axis=1).drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
+    # create sql engine
+    db_connection = ''.join(['sqlite:///', database_filename])
+    table_name = database_filename.split('.')[0]
+    engine = create_engine(db_connection)
+    if not engine.has_table(table_name):
+        df.to_sql(table_name, engine, index=False)
     pass
 
 
