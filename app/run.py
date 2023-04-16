5ea72bef79e09076a63ce8cdb4fe3ddc978be302
iaroslavm <iaroslav.miller@gmail.com>
2023-04-16 15:45:53 -0400

    comment 11: adding two additional visualizations

--- app/run.py
+++ app/run.py
@@ -43,6 +43,22 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
+    # calculate the frequency of the categories
+    categories_frequency = sum(df[df.columns[5:]].values)/len(df)*100
+    category_frequency_dict = dict(zip(df.columns[5:], categories_frequency))
+    categories_list = list(category_frequency_dict.keys())
+    freq_list = list(category_frequency_dict.values())
+    most_frequent_category = list(category_frequency_dict.keys())[0]
+
+    # calculate what categories correlate the most with most frequent category
+    corr_matrix = df[df.columns[6:]].corr()
+    # remove columns that are not correlated with others
+    corr_matrix = corr_matrix[corr_matrix.sum() != 0][corr_matrix.columns[corr_matrix.sum() != 0]]
+    corr_matrix = corr_matrix[most_frequent_category]
+    corr_matrix = corr_matrix.drop(most_frequent_category)
+    correlated_categories = list(corr_matrix.index)
+    correlations = list(corr_matrix.values)
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -61,6 +77,42 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=categories_list,
+                    y=freq_list
+                )
+            ],
+
+            'layout': {
+                'title': 'Frequency of Categories, percent',
+                'yaxis': {
+                    'title': "Percent"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=correlated_categories,
+                    y=correlations
+                )
+            ],
+
+            'layout': {
+                'title': ': '.join(['Category correlations with most frequent category', most_frequent_category]),
+                'yaxis': {
+                    'title': "Percent"
+                },
+                'xaxis': {
+                    'title': "Category"
                 }
             }
         }
